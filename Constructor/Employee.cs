8825ab78b3945bd664613792358fe2c123a5f0df
artir <artir@REYANSH2021>
2023-03-17 14:17:57 +0530

    Add project files.

---
+++ Constructor/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Employee
+    {
+
+        public Employee()
+        {
+            Id = 10;
+            Name = "Raaj";
+            Salary = 10000;
+        }
+
+        public Employee(int a,int b)
+        {
+
+        }
+
+        public void putdata()
+        {
+            //Console.WriteLine("Id:"+Id);
+            //Console.WriteLine("Name:"+Name);
+            //Console.WriteLine("Salary:"+Salary);
+
+        }
+        public void getdata()
+        {
+            //Console.WriteLine("Enter Employee Id:");
+            //Id = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Employee Name:");
+            //Name = Console.ReadLine();
+            //Console.WriteLine("Enter Employee salary:");
+            //Salary = Convert.ToInt32(Console.ReadLine());
+        }
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Salary { get; set; }
+
+
+       
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using Constructor;
+
+public class Program
+{
+    public static void Main()
+    {
+        int a, b;
+        int num;
+        string str;
+
+
+        Employee emp = new Employee();
+       
+        //emp.getdata();
+        //emp.putdata();
+
+    }
+}
